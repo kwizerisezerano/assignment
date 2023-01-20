138c39ac3a75a4607fc9cf0afa1e91dfd9e754a5
kwizerisezerano <123183945+kwizerisezerano@users.noreply.github.com>
2023-01-20 07:11:53 -0800

    Add files via upload

---
+++ information.php
@@ -0,0 +1,7 @@
+<?php
+    $name="<b><i>KWIZERISEZERANO</i></b>";
+    $regno="<b><i>21RP01262</i></b>";
+    echo "My name is"." ".$name;
+    echo "<br><br>";
+    echo "My regno is"."   ".$regno;
+?>
